2636f2318a8a823a343fd512a8c2ff0140ce560f
Rian Sanchez <Askoot94@gmail.com>
2026-01-25 16:44:29 -0500

    Abstracted Glossary Creation into function

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
+def createGlossary(filename):
+    # Need to grab Glossary of replacement terms from a file. And store in preprocess
+    
+    # Identify List of terms to replace
+    terms = list(dict())
+    
+    # with statement will handle opening and closing the file.
+    with open(filename, "rt", encoding="UTF-8") as glossaryFile:
+
+        # Grab a line from the file
+        for Line in glossaryFile:
+            Line = Line.strip()
+            # find '='
+            delim = Line.find("=")
+            split = dict(find =Line[:delim], replace =Line[delim+1:])
+            terms.append(split)
+    return terms
+
 # Line to translate
 translate = str("　クロウは原作主人公ことシロウと瓜二つの容姿をしている。\nシロウの肌を褐色にして髪色を銀にすればまんま俺だ。")
 
 # Storage for replaced Text
 send = str()
 
-# Need to grab Glossary of replacement terms from a file. And store in preprocess
-glossaryFile = open("./TestFiles/glossary.txt", "rt", encoding="UTF-8")
-
-# Identify List of dictionaries to replace
-terms = list(dict())
-
-# Grab a line from the 
-for Line in glossaryFile:
-    # find '='
-    delim = Line.find("=")
-    split = dict(find =Line[:delim], replace =Line[delim+1:])
-    terms.append(split)
-
-# Close file
-glossaryFile.close()
-
+# Create Glossary
+glossary = createGlossary("./TestFiles/glossary.txt")
 
 # Start Replacement
 for line in translate.split("\n"):
     # Identify and store character(s) to remove
     # Identify and Store character(s) to insert
-    for term in terms:
+    for term in glossary:
         line = line.replace(term["find"],term["replace"])
     send = send + "\n" + line
 
-# Return output to console
+# Replacement for Translation
 print(translate)
 print(send)
+
