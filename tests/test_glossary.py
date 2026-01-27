428567a3ac43032b409202043ee2a67b2bdb471b
Rian Sanchez <Askoot94@gmail.com>
2026-01-26 23:10:52 -0500

    Allowed Tests to access testdata files.

--- tests/test_glossary.py
+++ tests/test_glossary.py
@@ -1,33 +1,36 @@
 import pytest
+import os
 from .. import tools
 
-# change this to a fixture
-test_dir = "./glossary_testfiles/"
+@pytest.fixture(autouse=True)
+def directory():
+    return os.path.dirname(os.path.abspath(__file__)) + "\\glossary_testfiles\\"
+
 
 # Test that given a proper formatted 1 equal sign per line
-def test_goodcase():
+def test_goodcase(directory):
     # Arrange
-    expected_result = [{"find":"クロウ", "replace":"Kuro"},{"find":"クロウ", "replace":"Kuro"}]
+    expected_result = [{"find":"クロウ", "replace":"Kuro"},{"find":"シロウ", "replace":"Shiro"}]
     
     # Act
-    actual = tools.glossary.createGlossary(test_dir + "goodcase.txt")
+    actual = tools.glossary.createGlossary(os.path.join(directory, "goodcase.txt"))
 
     # Assert
     assert expected_result == actual
 
 # Tests that the function throws an error message saying terms are not seperated by newlines
-def test_multiterm_case():
+def test_multiterm_case(directory):
     with pytest.raises(Exception):
-        tools.glossary.createGlossary(test_dir + "multiterm.txt")
+        tools.glossary.createGlossary(os.path.join(directory, "multiterm.txt"))
 
-# Tests that it will not break if given a empty file
-def test_empty_case():
+# Tests that it will not break if given an empty file
+def test_empty_case(directory):
     # Arrange
     expected_result = list(dict())
 
     # Act
     try:
-        actual = tools.glossary.createGlossary(test_dir + "empty.txt")
+        actual = tools.glossary.createGlossary(os.path.join(directory, "empty.txt"))
 
     except:
         assert False
