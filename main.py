6388962407de7dd74c3b115df4398c7979731eef
Rian Sanchez <Askoot94@gmail.com>
2026-01-27 17:10:45 -0500

    + Removed tests import from main +Moved Glossary replacment into tools

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
 import tools
-import tests
 
 # Line to translate
-translate = str("　クロウは原作主人公ことシロウと瓜二つの容姿をしている。\nシロウの肌を褐色にして髪色を銀にすればまんま俺だ。")
-
-# Storage for replaced Text
-send = str()
+base_text = str("　クロウは原作主人公ことシロウと瓜二つの容姿をしている。\nシロウの肌を褐色にして髪色を銀にすればまんま俺だ。")
 
 # Create Glossary
 glossary = tools.glossary.createGlossary("./Example Files/glossary.txt")
 
 # Start Replacement
-for line in translate.split("\n"):
-    # Identify and store character(s) to remove
-    # Identify and Store character(s) to insert
-    for term in glossary:
-        line = line.replace(term["find"],term["replace"])
-    send = send + "\n" + line
+replaced = tools.glossary.replaceTerms(base_text, glossary)
 
 # Replacement for Translation
-print(translate)
-print(send)
+print(replaced)
 
--- tools/glossary.py
+++ tools/glossary.py
@@ -1,17 +1,45 @@
-def createGlossary(filename):
+def createGlossary(filename:str):
     # Need to grab Glossary of replacement terms from a file. And store in preprocess
     
     # Identify List of terms to replace
     terms = list(dict())
     
-    # with statement will handle opening and closing the file.
-    with open(filename, "rt", encoding="UTF-8") as glossaryFile:
+    # try to open file
+    try:
+        # with statement will handle opening and closing the file.
+        with open(filename, "rt", encoding="UTF-8") as glossaryFile:
 
-        # Grab a line from the file
-        for Line in glossaryFile:
-            Line = Line.strip()
-            # find '='
-            delim = Line.find("=")
-            split = dict(find =Line[:delim], replace =Line[delim+1:])
-            terms.append(split)
-    return terms
+            # Grab a line from the file
+            for Line in glossaryFile:
+                # remove trailing newline on right side
+                Line = Line.rstrip()
+                
+                # find '='
+                delim = Line.find("=")
+ 
+                # Identify and Store character(s) to remove and insert
+                split = dict(find =Line[:delim], replace =Line[delim+1:])
+                
+                # add the new dictionary to list
+                terms.append(split)
+        return terms
+    except FileNotFoundError as e:
+        e.add_note("Requested file was not found.")
+        return terms
+
+def replaceTerms(text: str, glossary:list):
+    if glossary != [] and text != "":
+        send = str()
+        for line in text.split("\n"):
+            for term in glossary:
+                # Check for find and replace terms in given dict
+                if "find" and "replace" in term:    
+                    line = line.replace(term["find"],term["replace"])
+                    send = send + '\n' + line
+                else:
+                    print("Bad Dict recieved")
+                    print(term)
+                    
+        return send
+    else:
+        return text
